d9493e735c4c115155bb289fe291a226632f99a6
Your Name <you@example.com>
2024-04-02 00:43:16 +0530

    modified:   app/[id]/page.tsx

--- app/[id]/page.tsx
+++ app/[id]/page.tsx
@@ -1,5 +1,6 @@
 import fetchPostById from "@/actions/fetchPostById";
 import ShowSingleBlog from "@/components/ShowSingleBlog";
+import { type BlogProps } from "@/models/blogs";
 
 type Params = {
     params: {
@@ -8,11 +9,11 @@ type Params = {
 };
 
 export default async function SingleBlogPage({ params: { id } }: Params) {
-    const post = await fetchPostById(id);
+    const post = (await fetchPostById(id)) as BlogProps;
 
     return (
         <div className="min-h-dvh px-4 pt-20 pb-4 flex flex-col justify-start items-center w-full">
-            <ShowSingleBlog id={id} post={post} />
+            <ShowSingleBlog post={post} />
         </div>
     );
 }
